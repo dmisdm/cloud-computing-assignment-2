6996f12372c1d782ea81f08d469196db64420c68
Daniel Manning <danielmanning213@gmail.com>
2021-04-30 10:30:55 +1000

    more

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 rsync -rltvh --progress --exclude 'node_modules'  --exclude 'web/.next' . assignment2:~/app
 
-ssh assignment2 docker build -t app_server ./app && docker rm -f app_server && docker run -d --name app_server -p 80:80 app_server
+ssh assignment2 "cd app && sudo docker-compose build && sudo docker-compose up -d"
